e05938df9b0f0f9b94e990f8b508277d2b1f9294
Arshad Ali <gibbsali87@gmail.com>
2019-12-21 22:43:39 +0000

    Create SB-with-Blynk.py
    AdHoc operation with Blynk

---
+++ SB-with-Blynk.py
@@ -0,0 +1,15 @@
+import blynklib
+
+BLYNK_AUTH = 'txbM-LjJOrPMp3Beh-Ts1RBYGFMJ3Ahp'
+
+blynk = blynklib.Blynk(BLYNK_AUTH)
+
+WRITE_EVENT_PRINT_MSG = "[WRITE_VIRTUAL_PIN_EVENT] Pin: V{} Value: '{}'"
+
+@blynk.handle_event('write V1')
+def write_virtual_pin_handler(pin, value):
+    print(WRITE_EVENT_PRINT_MSG.format(pin, value))
+
+
+while True:
+    blynk.run()
