3dc83014a50b15c7fe45a6b167119daaec7253fa
Arshad Ali <gibbsali87@gmail.com>
2019-12-22 01:20:48 +0000

    Update SB-with-Blynk.py

--- SB-with-Blynk.py
+++ SB-with-Blynk.py
@@ -6,9 +6,14 @@ blynk = blynklib.Blynk(BLYNK_AUTH)
 
 WRITE_EVENT_PRINT_MSG = "[WRITE_VIRTUAL_PIN_EVENT] Pin: V{} Value: '{}'"
 
+
 @blynk.handle_event('write V1')
 def write_virtual_pin_handler(pin, value):
     print(WRITE_EVENT_PRINT_MSG.format(pin, value))
+    if value == [u'1']:
+        print("Blind Opened")
+    else:
+        print("Blinds Closed")
 
 
 while True:
